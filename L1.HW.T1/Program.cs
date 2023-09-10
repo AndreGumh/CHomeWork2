d107f7ca1ffcb74d3ea2d24d99ae1decc4226ceb
AndreGumh <andrewhumany@gmail.com>
2023-09-10 22:57:25 +0300

    - add homework 1

--- L1.HW.T1/Program.cs
+++ L1.HW.T1/Program.cs
@@ -5,3 +5,16 @@
 // a = -9 b = -3 -> max = -3
 
 
+Console.WriteLine("Введите число a");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a>b)
+{
+    Console.WriteLine("Число a больше числа b");
+}
+else
+{
+    Console.WriteLine("Число b больше числа a");
+}
