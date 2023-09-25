c7940d6fab311052c5c92e5c128144646fec0a1a
AndreGumh <andrewhumany@gmail.com>
2023-09-25 21:56:05 +0300

    Add Class Work Task 1

---
+++ CW3/CW3.T1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача №17. Напишите программу, которая принимает на вход
+// координаты точки (X и Y), причем X и Y не равны 0 и выдает
+// номер четверти плоскости, в которой находится эта точка.
+
+System.Console.WriteLine("Введите координату X");
+int x = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите координату Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+    System.Console.WriteLine("Это первая четверть");
+else if (x > 0 && y < 0)
+    System.Console.WriteLine("Это четвертая четверть");
+else if (x < 0 && y < 0)
+    System.Console.WriteLine("Это вторая четверть");
+else if (x < 0 && y > 0)
+    System.Console.WriteLine("Это вторая четверть");
+else
+    System.Console.WriteLine("Точка лежит на координатной оси");
